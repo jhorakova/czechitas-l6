11a538f289dafb1ba85696387ba0110ef0620dbb
Jana Horáková <117632354+jhorakova@users.noreply.github.com>
2023-02-28 14:50:14 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,98 @@
+// tady je místo pro náš program
+
+/*Cvičení 1
+Bez použití knihovny dayjs napište funkci parseDate, která na vstupu obdrží řetězec obsahující datum ve formátu DD.MM.YYYY a 
+vrátí objekt s jednotlivýnu částmi zadaného data. Příklad použití:
+
+> parseDate('12.03.2015')
+{ day: 12, month: 3, year: 2015 }
+> parseDate('06.04.2021')
+{ day: 6, month: 4, year: 2021 }*/
+
+
+/*const enterDate = prompt('Zadej libovolné datum ve formátu den.měsíc.rok: ')
+
+const parseDate = (enterDate) => {
+    return {
+        day: enterDate.slice(0,2),
+        month: enterDate.slice(3,5),
+        year: enterDate.slice(-4)
+    }
+}
+console.log(parseDate(enterDate))*/
+
+/*Cvičení 2
+Bez použití knihovny dayjs napište funkci formatDate, která na vstupu obdrží objekt představující datum v následujícím formátu.
+
+{ day: 12, month: 3, year: 2015 }
+Funkce vrátí řetězec představující datum ve formátu DD.MM.YYYY. Příklad použití.
+
+> formatDate({ day: 6, month: 4, year: 2021 })
+"06.04.2021"
+V tomto cvičení se vám jistě bude hodit metoda padStart. Zkuste také uvnitř těla funkce použít destrukturování.*/
+
+/*const enterDay = prompt('Zadej libovolny den v mesici: ')
+const enterMonth = prompt('Zadej libovolny mesic v roce: ')
+const enterYear = prompt('Zadej libovolny rok: ')
+
+const date = {
+    day: enterDay,
+    month: enterMonth,
+    year: enterYear
+}
+
+//console.log(date)
+
+const { day, month, year } = date
+
+const formatDate = (date) => {
+    return `
+    ${day.padStart(2,0)}.${month.padStart(2,0)}.${year.padStart(2,0)}
+      `
+}
+
+console.log(formatDate(date))*/
+
+/*Cvičení 3
+Jak jistě znalci jazyka Python dobře vědí, funkce round v tomto jazyce zaokrouhluje malinko jinak, než jsme zvyklí. 
+Pokud je desetinná část čísla přesně 0.5, Python zaokrouhluje k nejbližšímu sudému číslu. 
+JavaScriptová funkce Math.round naopak provádí zaokrouhlování způsobem, na který jste zvyklí, tedy pro 0.5 vždy nahoru. 
+Někomu by se po po Pythonovském zaokrouhlování mohlo stýskat. Napište proto funkci jménem round, 
+která bude zaokrouhlovat čísla na celé jednotky podle následujících pravidel.
+
+Pokud je desetinná část menší než 0.5, zaokrouhlujeme dolů.
+Pokud je desetinná část větší než 0.5, zaokrouhlujeme nahoru.
+Pokud je desetinná část přesně rovna 0.5, zaokrouhlujeme k sudému číslu. 
+Tedy například 3.5 se zaokrouhlí na 4, naopak 2.5 se zaokrouhlí na 2.
+V tomto cvičení se vám může hodit funkce Math.trunc, která umí odříznout desetinnou část čísla.
+
+Bonus
+Zajistěte, aby funkce správně fungovala i pro záporná čísla.
+
+Tedy například -3.5 se zaokrouhlí na -4, naopak -2.5 se zaokrouhlí na -2.*/
+
+const desetinneCislo = prompt('Zadej cislo s jednim desetinnym mistem, misto carky pouzij tecku: ')
+//console.log(desetinneCislo)
+
+const round = (desetinneCislo) => {
+    if (desetinneCislo.slice(-1) < 5) {
+        return (Math.trunc(desetinneCislo))
+    } else if (desetinneCislo.slice(-1) > 5) {
+        return (Math.trunc(desetinneCislo) + 1)
+    }
+        
+    }
+
+
+//console.log(Math.trunc(Number(desetinneCislo)))
+//console.log(desetinneCislo.slice(-1))
+console.log(round(desetinneCislo))
+
+
+
+
+
+
+
+
+
